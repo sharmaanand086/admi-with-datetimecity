411d5a622f71137ab18fd80d297d5a435c6ed129
sharmaanand086 <38725515+sharmaanand086@users.noreply.github.com>
2019-03-13 12:22:20 +0530

    Add files via upload

---
+++ adminator.sql
@@ -0,0 +1,108 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 07, 2018 at 03:00 PM
+-- Server version: 10.1.32-MariaDB
+-- PHP Version: 5.6.36
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `adminator`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `adminlogin`
+--
+
+CREATE TABLE `adminlogin` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `adminlogin`
+--
+
+INSERT INTO `adminlogin` (`id`, `name`, `email`, `password`) VALUES
+(1, 'anand', 'anand@gmail.com', 'e6e061838856bf47e1de730719fb2609'),
+(2, 'limra', 'limra@gmail.com', 'admin@123');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_mgt`
+--
+
+CREATE TABLE `user_mgt` (
+  `id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `mobile` varchar(20) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `user_name` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `cpassword` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_mgt`
+--
+
+INSERT INTO `user_mgt` (`id`, `name`, `mobile`, `email`, `user_name`, `password`, `cpassword`) VALUES
+(2, 'anand shrma', '2147483647', 'anandsharma@gmail.com', 'anand', '123456', '123456'),
+(3, 'anand shrma', '8882298643', 'anandsharma1@gmail.com', 'anand1', '123456', '123456'),
+(4, 'anand shrma', '8882298643', 'anandsharma12@gmail.com', 'anand12', '123456', '123456'),
+(5, 'anand shrma', '8882298643', 'anandsharma123@gmail.com', 'anand123', '123456', '123456'),
+(6, 'anoop', '986889755', 'anoop@gmail.com', 'anoop', '123456', '123456');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `adminlogin`
+--
+ALTER TABLE `adminlogin`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `user_mgt`
+--
+ALTER TABLE `user_mgt`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `adminlogin`
+--
+ALTER TABLE `adminlogin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user_mgt`
+--
+ALTER TABLE `user_mgt`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
